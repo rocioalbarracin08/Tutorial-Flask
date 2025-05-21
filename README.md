3c7e3b89ffc1bc06c176a1e01c71a9c5aa0442f8
Rocio <ralbarracintrab@gmail.com>
2025-05-21 09:48:56 -0300

    Blpg blueprint archivos agregados para interactuar con el post de forma dinámica y estética

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Tutorial-Flask
 Creación de una aplicación básica
+
+Carpeta "flaskr" -> Fábrica de aplicaciones
+
+/create y register: funcionan igual
+
+Actualizacion - update y delete -> vistas (necesitarán obtener un post por id y comprobar si el autor coincide con el usuario conectado)
+
---
+++ flaskr/blog.py
@@ -0,0 +1,101 @@
+#-------------------EL BLUEPRINT----------------------
+
+# Lo que hace es llevar a la página inicial dónde se encuentran los botones de log in o registrarse (estas 2 se manejan con rutas) - Si me logue con mi cuenta ya registrada me vuelve mandar a la página inicial (pero esta vez ya logueada)
+
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, url_for
+)
+from werkzeug.exceptions import abort
+
+from flaskr.auth import login_required
+from flaskr.db import get_db
+
+bp = Blueprint('blog', __name__)
+
+@bp.route('/')
+def index():
+    db = get_db()
+    posts = db.execute(
+        'SELECT p.id, title, body, created, author_id, username'
+        ' FROM post p JOIN user u ON p.author_id = u.id'
+        ' ORDER BY created DESC'
+    ).fetchall()
+    return render_template('blog/index.html', posts=posts)
+
+@bp.route('/create', methods=('GET', 'POST'))
+@login_required
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        body = request.form['body']
+        error = None
+
+        if not title:
+            error = 'Title is required.'
+
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                'INSERT INTO post (title, body, author_id)'
+                ' VALUES (?, ?, ?)',
+                (title, body, g.user['id'])
+            )
+            db.commit()
+            return redirect(url_for('blog.index'))
+
+    return render_template('blog/create.html')
+
+def get_post(id, check_author=True): #check_author se define para que la función pueda ser utilizada para obtener un post sin comprobar el autor.
+    post = get_db().execute(
+        'SELECT p.id, title, body, created, author_id, username'
+        ' FROM post p JOIN user u ON p.author_id = u.id'
+        ' WHERE p.id = ?',
+        (id,)
+    ).fetchone()
+
+    if post is None:
+        abort(404, f"Post id {id} doesn't exist.") # Toma un mensaje opcional(en los paréntesis) para mostrar con el error, de lo contrario se utiliza un mensaje por defecto. Ejem: 404. 
+
+    if check_author and post['author_id'] != g.user['id']:
+        abort(403)
+
+    return post
+
+@bp.route('/<int:id>/update', methods=('GET', 'POST'))
+@login_required
+def update(id):
+    post = get_post(id)
+
+    if request.method == 'POST':
+        title = request.form['title']
+        body = request.form['body']
+        error = None
+
+        if not title:
+            error = 'Title is required.'
+
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                'UPDATE post SET title = ?, body = ?'
+                ' WHERE id = ?',
+                (title, body, id)
+            )
+            db.commit()
+            return redirect(url_for('blog.index'))
+
+    return render_template('blog/update.html', post=post)
+
+# --------------BORRAR--------------
+@bp.route('/<int:id>/delete', methods=('POST',))
+@login_required
+def delete(id):
+    get_post(id)
+    db = get_db()
+    db.execute('DELETE FROM post WHERE id = ?', (id,))
+    db.commit()
+    return redirect(url_for('blog.index'))
